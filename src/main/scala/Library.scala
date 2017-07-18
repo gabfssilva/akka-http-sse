cc109eed1118c9ea69de0d0d639be49db7cc302b
Gabriel <gabfssilva@gmail.com>
2017-07-18 12:00:26 -0300

    simple endpoint added

--- src/main/scala/Library.scala
+++ src/main/scala/Library.scala
@@ -1,53 +1,38 @@
+import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.event.Logging
 import akka.http.scaladsl.marshalling.sse.EventStreamMarshalling._
-import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.model.sse.ServerSentEvent
 import akka.http.scaladsl.server.{HttpApp, Route}
 import akka.stream.scaladsl.Source
 
-import scala.collection.mutable
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 import scala.concurrent.duration._
-import scala.util.Random
-
-import scala.util.{Success => ScalaSuccess}
+import scala.util.{Random, Success => ScalaSuccess}
 
 object EventsApp extends HttpApp {
-  val resources: mutable.Set[Int] = mutable.Set()
+  var list = Seq(1)
 
   implicit val theSystem = ActorSystem(Logging.simpleName(this).replaceAll("\\$", ""))
-  theSystem.scheduler.schedule(0.seconds, 10.seconds, () => resources.add(Random.nextInt(1000)))
+  theSystem.scheduler.schedule(0.seconds, 2.seconds, () => list = list :+ Random.nextInt(1000))
 
   override def routes: Route =
     pathPrefix("api") {
-      path("events" / Segment) { event =>
+      path("events" / IntNumber) { from =>
         get {
           complete {
+            var last = from - 1
+
             Source
-              .fromGraph(new RandomSource)
-              .map(number => ServerSentEvent(number.toString, event))
-              .keepAlive(2.second, () => ServerSentEvent.heartbeat)
-          }
-        }
-      }
-
-      path("resource") {
-        get {
-          def result: Future[Option[String]] = Future {
-            if (resources.isEmpty) {
-              None
-            } else {
-              val head = resources.head
-              resources.remove(head)
-              Some(head.toString)
-            }
-          }
-
-          onComplete(result) {
-            case ScalaSuccess(Some(x)) => complete(x)
-            case ScalaSuccess(None) => complete(NotFound)
+              .tick(0 seconds, 200 millis, NotUsed)
+              .filter(_ => last <= list.size)
+              .map { _ =>
+                val result = list(last)
+                last = last + 1
+                result
+              }
+              .map(number => ServerSentEvent(number.toString))
+              .keepAlive(10.second, () => ServerSentEvent.heartbeat)
           }
         }
       }
--- src/main/scala/RandomSource.scala
+++ src/main/scala/RandomSource.scala
@@ -4,26 +4,37 @@ import akka.stream.{Attributes, Outlet, SourceShape}
 import akka.stream.stage.{AbstractOutHandler, GraphStage, GraphStageLogic}
 
 import scala.concurrent.duration._
-
 import scala.util.Random
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
   * @author Gabriel Francisco <gabfssilva@gmail.com>
   */
-class RandomSource extends GraphStage[SourceShape[Int]]{
+class RandomSource(from: Int) extends GraphStage[SourceShape[Int]]{
+  var list = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  var last = from - 1
+
+  implicit val theSystem = ActorSystem(Logging.simpleName(this).replaceAll("\\$", ""))
+  theSystem.scheduler.schedule(0.seconds, 2.seconds, () => list = random :: list)
+
   val out: Outlet[Int] = Outlet.create("RandomSource.out")
   val sourceShape: SourceShape[Int] = SourceShape.of(out)
 
   override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = {
     new GraphStageLogic(shape){
       setHandler(out, new AbstractOutHandler {
-        override def onPull(): Unit = push(out, random)
+        override def onPull(): Unit = if (last < list.size) {
+          push(out, list(last))
+          last = last + 1
+        } else {
+          setKeepGoing(true)
+        }
       })
     }
   }
 
   def random: Int = {
-    Thread.sleep(1000)
     Random.nextInt(100)
   }
 
